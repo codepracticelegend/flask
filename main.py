4565d4e577966b415811056a2fa310b91693840a
codepracticelegend <90989389+codepracticelegend@users.noreply.github.com>
2024-10-10 15:58:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 import os
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'Beta'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('chat.html')
 
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Message: {msg}")
+    send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    # Get the port from the environment (default to 5000 if not provided)
+    port = int(os.getenv('PORT', 5000))
+    socketio.run(app, debug=True, host='0.0.0.0', port=port)
